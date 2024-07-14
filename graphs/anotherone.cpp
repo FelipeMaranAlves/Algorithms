6971819562283b14080de20a0217415868eb711b
Felipe Alves <felipemaranalvees40@gmail.com>
2024-07-13 23:08:23 -0300

    routine commit, to make my git prettier

--- graphs/anotherone.cpp
+++ graphs/anotherone.cpp
@@ -3,31 +3,91 @@ using namespace std;
 
 #define VISITED 1
 #define UNVISITED 0
-
 class Edges
 {
 private:
     int wt;
     int child;
 public:
-    Edges();
-    ~Edges();
+    Edges(int w,int c){ wt = w; child = c;}
+    int GetWeight(){return wt;}
+    int GetChild(){return child;}
 };
 
-class matrixList
+// class Vertex
+// {
+// private:
+//     int state;
+//     //possibly a name (if the name is a string i could store the hash function that generates the index of it's position in the Mark array(wich now is an overall storage for information))
+// public:
+//     Vertex(int vis){ state = vis;}
+//     void setState(int a){state = a;}
+// };
+
+
+
+class GraphList
 {
 private:
-    int* Mark;
-    list<Edges>* adjList;
+    int* Mark; // the array for the representation of states
+    list<Edges>* adjList; 
     int numVertex;
 
 public:
-    matrixList() {};
-    ~matrixList();
+    GraphList(int n){
+        // Mark = (int*)malloc(sizeof(int)*n);
+        // adjList = (list<Edges>*)malloc(sizeof(list<Edges>)*n); // estou assumindo que inicializa com null certin ou size 0 enfim
+        Mark = new int[n] ;
+        adjList = new list<Edges>[n];
+        numVertex = n;
+        for (int i = 0; i < n; ++i) {
+            Mark[i] = UNVISITED;
+        }
+    };
+    ~GraphList(){
+        // free(Mark);
+        // free(adjList);
+        delete[] Mark;
+        delete[] adjList;
+    };
 
     int first(int v){
-        Edges arestas = adjList[v];
-
+        if (adjList[v].empty()) {return numVertex;};
+        return adjList[v].front().GetChild(); //esse cara
     }
+
+    int next(int v){
+        for (auto it : adjList[v])//(auto it = adjList[v].begin(); it != adjList[v].end(); ++it) outro jeito
+        {
+            // PAREI AQUI
+        }
+        
+    }
+
+    void setEdge(int i, int j,int wt){ //directed
+        Edges new_edge(wt,j);
+        adjList[i].push_back(new_edge);
+    }
+    void setMark(int v,int state){
+        if (-1< v < numVertex) { Mark[v] = state;}
+    }
+    int getMark(int v){return Mark[v];}
+
+
+
+    void BFS(int start){
+        queue<int> q;
+        q.push(start);
+        Mark[start] = VISITED;
+        while (/* condition */)
+        {
+            /* code */
+        }
+        
+    }
+    // void BFS(int start);
+    // void graphTraverse();
+    // void clearGraph();
+    
 };
 
