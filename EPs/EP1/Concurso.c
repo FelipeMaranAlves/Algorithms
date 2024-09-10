ba021338da4bf4bd40efd9f6819bdc04384c49ab
Felipe <felipemaranalvees40@gmil.com>
2024-09-10 16:24:40 -0300

    organising

---
+++ EPs/EP1/Concurso.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+//usar o merge por ser estavel, pra fazer as ordens
+int main(){
+    int cargos;
+    scanf("%d",&cargos);
+    for (int i=0;i<cargos;i++){
+        int inscritos;
+        int vagas;
+        scanf("%d %d",inscritos, vagas);
+        for (int j =0;j<inscritos;j++){
+            
+            scanf("%s")
+        }
+    }
+    return 0;
+}
+
+// gcc Concurso.c -o Concurso.exe ; ./Concurso.exe
